89821e1d1c6dbbad16e29549bae76fc6bdb61c41
nuhptr <nugrahaadi733@gmail.com>
2023-09-03 21:04:37 +0700

    update drag and drop code update in db

--- components/Board.tsx
+++ components/Board.tsx
@@ -11,9 +11,13 @@ import { useBoardStore } from '@/store/BoardStore'
 import Column from './Column'
 
 function Board() {
-  const [board, getBoard, setBoardState] = useBoardStore(
-    (state) => [state.board, state.getBoard, state.setBoardState]
-  )
+  const [board, getBoard, setBoardState, updateTodoInDB] =
+    useBoardStore((state) => [
+      state.board,
+      state.getBoard,
+      state.setBoardState,
+      state.updateTodoInDB,
+    ])
 
   useEffect(() => {
     getBoard()
@@ -32,10 +36,7 @@ function Board() {
       entries.splice(destination.index, 0, removed)
 
       const rearrangedColumns = new Map(entries)
-      setBoardState({
-        ...board,
-        columns: rearrangedColumns,
-      })
+      setBoardState({ ...board, columns: rearrangedColumns })
     }
 
     // this step is needed as the indexes are stored as numbers 0, 1, 2, etc. instead of id's with DND Library
@@ -52,8 +53,52 @@ function Board() {
       id: finishColIndex[0],
       todos: finishColIndex[1].todos,
     }
+    console.log(startCol, finishCol)
 
     if (!startCol || !finishCol) return
+
+    if (
+      source.index === destination.index &&
+      startCol === finishCol
+    ) {
+      return
+    }
+
+    const newTodos = startCol.todos
+    const [todoMoved] = newTodos.splice(source.index, 1)
+
+    if (startCol.id === finishCol.id) {
+      // Same column task drag
+      newTodos.splice(destination.index, 0, todoMoved)
+      const newColumn = {
+        id: startCol.id,
+        todos: newTodos,
+      }
+      const newColumns = new Map(board.columns)
+      newColumns.set(startCol.id, newColumn)
+
+      setBoardState({ ...board, columns: newColumns })
+    } else {
+      // Different column task drag
+      const finishTodos = Array.from(finishCol.todos)
+      finishTodos.splice(destination.index, 0, todoMoved)
+
+      const newColumns = new Map(board.columns)
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      }
+      newColumns.set(startCol.id, newCol)
+      newColumns.set(finishCol.id, {
+        id: finishCol.id,
+        todos: finishTodos,
+      })
+
+      // update in db
+      updateTodoInDB(todoMoved, finishCol.id)
+
+      setBoardState({ ...board, columns: newColumns })
+    }
   }
 
   return (
@@ -78,7 +123,6 @@ function Board() {
                 />
               )
             )}
-            {provided.placeholder}
           </div>
         )}
       </Droppable>
