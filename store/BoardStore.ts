aa29add676e276d635e7458c0c0d873c69e54d12
nuhptr <nugrahaadi733@gmail.com>
2023-09-03 21:03:15 +0700

    update boardstore

--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,10 +1,15 @@
 import { create } from 'zustand'
 import { getTodosGrupedByColumn } from '@/utils/getTodosGroupedByColumn'
+import { databases } from '@/appwrite'
 
 interface BoardState {
   board: Board
   getBoard: () => void
   setBoardState: (board: Board) => void
+  updateTodoInDB: (todo: Todo, columnId: TypeColumn) => void
+
+  searchString: string
+  setSearchString: (searchString: string) => void
 }
 
 export const useBoardStore = create<BoardState>((set) => ({
@@ -14,4 +19,19 @@ export const useBoardStore = create<BoardState>((set) => ({
     set({ board })
   },
   setBoardState: (board) => set({ board }),
+  updateTodoInDB: async (todo, columnId) => {
+    await databases.updateDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
+      todo.$id,
+      {
+        title: todo.title,
+        status: columnId,
+      }
+    )
+  },
+
+  // search state
+  searchString: '',
+  setSearchString: (searchString) => set({ searchString }),
 }))
