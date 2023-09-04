049d02956b3b3483f0cf7b827c7286b7d0b98257
nuhptr <nugrahaadi733@gmail.com>
2023-09-04 10:17:41 +0700

    format todo for ai

---
+++ utils/formatTodoForAI.ts
@@ -0,0 +1,23 @@
+const formatTodoForAI = (board: Board) => {
+  const todos = Array.from(board.columns.entries())
+
+  const flatArray = todos.reduce((map, [key, value]) => {
+    map[key] = value.todos
+    return map
+  }, {} as { [key in TypeColumn]: Todo[] })
+
+  /**
+   * Reduce to key: value(length) pairs
+   */
+  const flatArrayCounted = Object.entries(flatArray).reduce(
+    (map, [key, value]) => {
+      map[key as TypeColumn] = value.length
+      return map
+    },
+    {} as { [key in TypeColumn]: number }
+  )
+
+  return flatArrayCounted
+}
+
+export default formatTodoForAI
