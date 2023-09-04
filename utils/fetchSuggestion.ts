55f9481e4afaa990f8fbe7de7b25591e539e2df6
nuhptr <nugrahaadi733@gmail.com>
2023-09-04 10:17:31 +0700

    fetchsuggestion

---
+++ utils/fetchSuggestion.ts
@@ -0,0 +1,19 @@
+import formatTodoForAI from './formatTodoForAI'
+
+const fetchSuggestion = async (board: Board) => {
+  const todos = formatTodoForAI(board)
+  console.log('Formatted todos for AI', todos)
+
+  const response = await fetch('/api/generateSummary', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ todos }),
+  })
+
+  const GPTData = await response.json()
+  const { content } = GPTData
+
+  return content
+}
+
+export default fetchSuggestion
