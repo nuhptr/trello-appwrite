aae56f71e8f93c7e3427e9a296489249d0e7398a
nuhptr <nugrahaadi733@gmail.com>
2023-09-04 16:20:45 +0700

    func: add functionality add image

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,8 +1,13 @@
+import { useEffect, useState } from 'react'
 import { XCircleIcon } from '@heroicons/react/24/solid'
 import {
   DraggableProvidedDragHandleProps,
   DraggableProvidedDraggableProps,
 } from 'react-beautiful-dnd'
+
+import { useBoardStore } from '@/store/BoardStore'
+import getUrl from '@/utils/getUrl'
+import Image from 'next/image'
 
 interface TodoProps {
   todo: Todo
@@ -10,10 +15,7 @@ interface TodoProps {
   id: TypeColumn
   innerRef: (element: HTMLElement | null) => void
   draggableProps: DraggableProvidedDraggableProps
-  dragHandleProps:
-    | DraggableProvidedDragHandleProps
-    | null
-    | undefined
+  dragHandleProps: DraggableProvidedDragHandleProps | null | undefined
 }
 
 function TodoCard({
@@ -24,6 +26,20 @@ function TodoCard({
   draggableProps,
   dragHandleProps,
 }: TodoProps) {
+  const [deleteTask] = useBoardStore((state) => [state.deleteTask])
+  const [imageURL, setImageURL] = useState<string | null>(null)
+
+  useEffect(() => {
+    if (todo.image) {
+      const fetchImage = async () => {
+        const url = await getUrl(todo.image!)
+        if (url) setImageURL(url.toString())
+      }
+
+      fetchImage()
+    }
+  }, [todo])
+
   return (
     <div
       className='bg-white rounded-md space-y-2 drop-shadow-md'
@@ -32,12 +48,25 @@ function TodoCard({
       ref={innerRef}>
       <div className='flex justify-between items-center p-5'>
         <p>{todo.title}</p>
-        <button className='text-red-500 hover:text-red-600'>
+        <button
+          onClick={() => deleteTask(index, todo, id)}
+          className='text-red-500 hover:text-red-600'>
           <XCircleIcon className='ml-5 h-8 w-8' />
         </button>
       </div>
 
       {/* Add Image Here */}
+      {imageURL && (
+        <div className='h-full w-full rounded-b-md'>
+          <Image
+            src={imageURL}
+            alt='Task Image'
+            width={400}
+            height={200}
+            className='w-full object-contain rounded-b-md'
+          />
+        </div>
+      )}
     </div>
   )
 }
