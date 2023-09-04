ea3c8b209891a4a1aca00b537bbc12237c7f8025
nuhptr <nugrahaadi733@gmail.com>
2023-09-04 09:29:39 +0700

    generateSummary openai

--- app/api/generateSummary/route.ts
+++ app/api/generateSummary/route.ts
@@ -1,9 +1,43 @@
+import openai from '@/openai'
 import { NextResponse } from 'next/server'
 
 export async function POST(request: Request) {
-  // todos in the body of the request
+  /**
+   * Get todos from the request body
+   */
   const { todos } = await request.json()
   console.log(todos)
 
-  // communicate with openAI GPT
+  /**
+   * Create a completion using the engine specified in the model parameter.
+   */
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    temperature: 0.8,
+    n: 1,
+    stream: false,
+    messages: [
+      {
+        role: 'system',
+        content: `When responding, welcome the user always as Mr. Adi and say welcome to TRELLO! limit the response to 20 characters.`,
+      },
+      {
+        role: 'user',
+        content: `Hi there, provided a summary of the following todos. Count how many todos are in each category such as To do, in progress and done, then tell the user to have a productive day! Here's the data: ${JSON.stringify(
+          todos
+        )}`,
+      },
+    ],
+  })
+
+  /**
+   * new response format for getting data from openai
+   * instead of using { data }, now we access the data directly
+   */
+  const { created, model, choices } = response
+
+  console.log(created, model, choices)
+  console.log(choices[0].message)
+
+  return NextResponse.json(choices[0].message)
 }
