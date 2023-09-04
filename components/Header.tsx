d98d6b0bfe10a394642161204cb6e8a8557119d2
nuhptr <nugrahaadi733@gmail.com>
2023-09-04 10:18:22 +0700

    add useEffect

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,20 +2,40 @@
 
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
-import React from 'react'
+import { useEffect, useState } from 'react'
 import {
   MagnifyingGlassIcon,
   UserCircleIcon,
 } from '@heroicons/react/24/solid'
 import Avatar from 'react-avatar'
+
 import { useBoardStore } from '@/store/BoardStore'
+import fetchSuggestion from '@/utils/fetchSuggestion'
 
 function Header() {
   const router = useRouter()
-  const [searchString, setSearchString] = useBoardStore((state) => [
-    state.searchString,
-    state.setSearchString,
-  ])
+  const [board, searchString, setSearchString] = useBoardStore(
+    (state) => [
+      state.board,
+      state.searchString,
+      state.setSearchString,
+    ]
+  )
+  const [loading, setLoading] = useState<boolean>(false)
+  const [suggestion, setSuggestion] = useState<string>('')
+
+  useEffect(() => {
+    if (board.columns.size === 0) return
+    setLoading(true)
+
+    const fetchSuggestionFunc = async () => {
+      const suggestion = await fetchSuggestion(board)
+      setSuggestion(suggestion)
+      setLoading(false)
+    }
+
+    fetchSuggestionFunc()
+  }, [board])
 
   return (
     <header>
@@ -63,8 +83,14 @@ function Header() {
 
       <div className='flex items-center justify-center px-5 md:py-5'>
         <p className='flex items-center p-5 text-sm font font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-xl text-[#0055d1]'>
-          <UserCircleIcon className='inline-block h-10 w-10 text-[#0055d1] mr-1' />
-          GPT is summarising your tasks for the day...
+          <UserCircleIcon
+            className={`inline-block h-10 w-10 text-[#0055d1] mr-1 ${
+              loading && 'animate-spin'
+            }`}
+          />
+          {suggestion && !loading
+            ? suggestion
+            : 'GPT is summarising your tasks for the day...'}
         </p>
       </div>
     </header>
