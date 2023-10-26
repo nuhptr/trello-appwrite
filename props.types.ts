a19c0a282d18050da8113396747be5fe08395889
nuhptr <nugrahaadi733@gmail.com>
2023-10-27 01:08:53 +0700

    update type definitions

--- props.types.ts
+++ props.types.ts
@@ -2,7 +2,7 @@ interface Board {
    columns: Map<TypeColumn, Column>
 }
 
-type TypeColumn = 'todo' | 'inprogress' | 'done'
+type TypeColumn = "todo" | "inprogress" | "done"
 
 interface Column {
    id: TypeColumn
